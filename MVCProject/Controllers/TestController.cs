607d8353206ebc9981e805667da0e26a25159e7f
Nikhil Karanjkar <karanjkarnikhil4@gmail.com>
2017-08-02 20:18:04 +0530

    First Commit

---
+++ MVCProject/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string  GetString()
+        {
+            return "Hello this is my first mvc project";
+        }
+    }
+}
